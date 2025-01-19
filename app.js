9499d69388a15436e394bdd0b99a819048429ac1
razyneko <139580532+INFERNO1211@users.noreply.github.com>
2025-01-19 22:52:04 +0530

    imporoved the code

--- app.js
+++ app.js
@@ -1,39 +1,66 @@
-const player1Score = document.querySelector('#player1Score');
-const player2Score = document.querySelector('#player2Score');
-const player1Button = document.querySelector('#player1Btn');
-const player2Button = document.querySelector('#player2Btn');
+const p1 = {
+    value: 0,
+    score : document.querySelector('#player1Score'),
+    button: document.querySelector('#player1Btn'),
+}
+
+const p2 = {
+    value: 0,
+    score: document.querySelector('#player2Score'),
+    button: document.querySelector('#player2Btn'),
+}
+
+// const player1Score = document.querySelector('#player1Score');
+// const player2Score = document.querySelector('#player2Score');
+// const player1Button = document.querySelector('#player1Btn');
+// const player2Button = document.querySelector('#player2Btn');
+
 const resetButton = document.querySelector('#resetBtn');
 const options = document.querySelector('#options');
 
 
 
-let p1Score = 0;
-let p2Score = 0;
+
 let winningScore = 5;
 let isGameOver = false;
 
-player1Button.addEventListener('click', function() {
-    if (!isGameOver) {
-        p1Score += 1;
-        if (p1Score === winningScore) {
+function updateScores(player, opponent) {
+     if (!isGameOver) {
+        player.value += 1;
+        if (player.value === winningScore) {
         isGameOver = true;
-        player1Score.style.color = 'green';
-        player2Score.style.color = 'red';
+        player.score.style.color = 'green';
+        opponent.score.style.color = 'red';
         }
-        player1Score.textContent = p1Score;
+        player.score.textContent = player.value;
     }
+}
+
+p1.button.addEventListener('click', function() {
+    
+    updateScores(p1,p2);
+    // if (!isGameOver) {
+    //     p1Score += 1;
+    //     if (p1Score === winningScore) {
+    //     isGameOver = true;
+    //     player1Score.style.color = 'green';
+    //     player2Score.style.color = 'red';
+    //     }
+    //     player1Score.textContent = p1Score;
+    // }
 });
 
-player2Button.addEventListener('click', function() {
-    if (!isGameOver) {
-        p2Score += 1;
-        if (p2Score === winningScore) {
-        isGameOver = true;
-        player2Score.style.color = 'green';
-        player1Score.style.color = 'red';
-        }
-        player2Score.textContent = p2Score;
-    }
+p2.button.addEventListener('click', function() {
+    updateScores(p2,p1);
+    // if (!isGameOver) {
+    //     p2Score += 1;
+    //     if (p2Score === winningScore) {
+    //     isGameOver = true;
+    //     player2Score.style.color = 'green';
+    //     player1Score.style.color = 'red';
+    //     }
+    //     player2Score.textContent = p2Score;
+    // }
 });
 
 options.addEventListener('change', (e) => {
@@ -41,14 +68,21 @@ options.addEventListener('change', (e) => {
     reset();
 })
 
-resetButton.addEventListener('click', reset)
 
 function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    player1Score.textContent = 0;
-    player2Score.textContent = 0;
-    player1Score.style.color = '';
-    player2Score.style.color = '';
+
+    for (let p of [p1,p2]) {
+        p.value = 0;
+        p.score.textContent = 0;
+        p.score.style.color = '';
+    }
+    // p1.value = 0;
+    // p2.value = 0;
+    // p1.score.textContent = 0;
+    // p2.score.textContent = 0;
+    // p1.score.style.color = '';
+    // p2.score.style.color = '';
 }
+
+resetButton.addEventListener('click', reset)
